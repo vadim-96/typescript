054da94d2ec12579d3dc6a5af3624c73c5fdeb6d
Vadim <vadjcool@gmail.com>
2019-09-17 23:05:55 +0500

    Delete app.ts

--- app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function cbFunction(text) {
-    console.log(`inside cbFunction ${text}`);
-}
-function doSomethingWithACallback(initialText, callback) {
-    console.log(`inside doSomethingWithCallback ${initialText}`);
-    callback(initialText);
-}
-doSomethingWithACallback('myText', cbFunction);
